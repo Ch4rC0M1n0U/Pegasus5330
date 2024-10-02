527a646b6496591ca39df749fdca2786dd0c6cc6
Ch4rC0M1n0U <gaetanminnekeer@gmail.com>
2024-10-03 01:32:34 +0200

    Ok pour rôles

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -15,65 +15,92 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class UserResource extends Resource
 {
-    protected static ?string $model = User::class;
-
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
-
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('email')
-                    ->email()
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\DateTimePicker::make('email_verified_at'),
-                Forms\Components\TextInput::make('password')
-                    ->password()
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\Textarea::make('two_factor_secret')
-                    ->columnSpanFull(),
-                Forms\Components\Textarea::make('two_factor_recovery_codes')
-                    ->columnSpanFull(),
-                Forms\Components\DateTimePicker::make('two_factor_confirmed_at'),
-                Forms\Components\TextInput::make('current_team_id')
-                    ->numeric(),
-                Forms\Components\TextInput::make('profile_photo_path')
-                    ->maxLength(2048),
-            ]);
-    }
-
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('name')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('email')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('email_verified_at')
-                    ->dateTime()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('two_factor_confirmed_at')
-                    ->dateTime()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('current_team_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('profile_photo_path')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+        protected static ?string $model = User::class;
+    
+        protected static ?string $navigationIcon = 'heroicon-o-user';
+        protected static ?string $navigationLabel = 'Utilisateurs';
+        protected static ?string $navigationGroup = 'Configuration';
+        protected static ?string $navigationBadgeTooltip = 'Liste des utilisateurs et gestion';
+    
+        public static function getNavigationBadge(): ?string
+        {
+            return static::getModel()::count();
+        }
+    
+        public static function form(Form $form): Form
+        {
+            return $form
+                ->schema([
+                    Forms\Components\TextInput::make('name')
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('email')
+                        ->email()
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('services')
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('telUser')
+                        ->tel()
+                        ->required()
+                        ->maxLength(20)
+                        ->default(0),
+                    Forms\Components\TextInput::make('Matricule')
+                        ->required()
+                        ->maxLength(50),
+                    Forms\Components\Select::make('roles')
+                        ->relationship('roles', 'name')
+                        ->multiple()
+                        ->preload()
+                        ->searchable(),
+                    Forms\Components\DateTimePicker::make('email_verified_at')
+                        ->hiddenOn('edit'),
+                    Forms\Components\TextInput::make('password')
+                        ->password()
+                        ->hidden()
+                        ->maxLength(255),
+                    Forms\Components\Textarea::make('two_factor_secret')
+                        ->columnSpanFull()
+                        ->hidden(),
+                    Forms\Components\Textarea::make('two_factor_recovery_codes')
+                        ->columnSpanFull()
+                        ->disabled()
+                        ->hidden(),
+                    Forms\Components\DateTimePicker::make('two_factor_confirmed_at')
+                        ->hiddenOn('edit'),
+                    Forms\Components\TextInput::make('profile_photo_path')
+                        ->maxLength(2048)
+                        ->disabled(),
+    
+                ]);
+        }
+    
+        public static function table(Table $table): Table
+        {
+            return $table
+                ->columns([
+                    Tables\Columns\TextColumn::make('name')
+                        ->searchable()
+                        ->sortable(),
+                    Tables\Columns\TextColumn::make('email')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('services')
+                        ->searchable()
+                        ->sortable(),
+                    Tables\Columns\TextColumn::make('telUser')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('Matricule')
+                        ->searchable()
+                        ->sortable(),
+                    Tables\Columns\TextColumn::make('created_at')
+                        ->dateTime()
+                        ->sortable()
+                        ->toggleable(isToggledHiddenByDefault: true),
+                    Tables\Columns\TextColumn::make('updated_at')
+                        ->dateTime()
+                        ->sortable()
+                        ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
+use Laravel\Jetstream\HasProfilePhoto;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
@@ -19,6 +20,8 @@ class User extends Authenticatable
     use HasTeams;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use HasRoles;
+
 
     /**
      * The attributes that are mass assignable.
