16c0d19b9bc5271b2b2e9ad1c2b888f8bddae77e
Ch4rC0M1n0U <gaetanminnekeer@gmail.com>
2024-10-03 02:09:22 +0200

    Themes

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -7,7 +7,9 @@ use Filament\Panel;
 use Filament\Widgets;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Hasnayeen\Themes\ThemesPlugin;
 use Filament\Http\Middleware\Authenticate;
+use Hasnayeen\Themes\Http\Middleware\SetTheme;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
@@ -28,9 +30,23 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandName('Pegasus')
+            ->brandLogoHeight('2.0rem')
+            ->brandLogo(asset('images/logo.png'))
+            ->darkMode(false)
+            ->sidebarCollapsibleOnDesktop()
+            ->breadcrumbs(true)
+            ->favicon(asset('images/favicon.png'))
             ->colors([
-                'primary' => Color::Amber,
+                'danger' => Color::hex('#EF5350'),
+                'info' => Color::hex('#E3F2FD'),
+                'success' => Color::hex('#8BC34A'),
+                'warning' => Color::hex('#FFA726'),
             ])
+            ->font('Montserrat')
+
+            //->maxContentWidth(MaxWidth::Full)
+
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
@@ -42,7 +58,9 @@ class AdminPanelProvider extends PanelProvider
                 Widgets\FilamentInfoWidget::class,
             ])
             ->plugins([
-                FilamentShieldPlugin::make()
+                FilamentShieldPlugin::make(),
+                ThemesPlugin::make()
+                    
             ])
             ->middleware([
                 EncryptCookies::class,
@@ -54,6 +72,7 @@ class AdminPanelProvider extends PanelProvider
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
+                SetTheme::class
             ])
             ->authMiddleware([
                 Authenticate::class,
